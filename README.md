6a31431f164ceef8bb5ffb6693d378aa7ac05858
Maksim Luzik <maksim.luzik@gmail.com>
2025-07-03 18:00:14 +0300

    working dummies

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # MXM Adalo Components
+
+Make sure you have latest Adalo CLI running:
+
+`sudo npm install -g @adalo/cli`
+
+To run locally (needs Adalo webtool):
+
+`yarn start`
+
+More info and help: https://developers.adalo.com/docs/basics/introduction
--- src/components/Example/index.js
+++ src/components/Example/index.js
@@ -1,22 +1,24 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Button } from 'react-native'
 
 const Example = (props) => {
-	const { color, text } = props
+    const { color, text, buttonTitle, onButtonPress } = props
 
-	return(
-		<View style={styles.wrapper}>
-			<Text style={{ color }}>{text}</Text>
-		</View>
-	)
+    return (
+        <View style={styles.wrapper}>
+            <Text style={{ color }}>{text}</Text>
+            <Button title={buttonTitle} onPress={onButtonPress} />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-	wrapper: {
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'center',
-	}
+    wrapper: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+    },
 })
 
 export default Example
